f34721b870cc98bb6eb2e8b8b6738d2219982174
k-sutine <ksutin@renaissancecapital.com>
2022-09-26 19:23:08 -0400

    Init commit.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [979, 1],
+        itemsTablet: [768, 1],
+        pagination: true,
+        navigation: true,
+        navigationText: ["", ""],
+        slideSpeed: 1000,
+        autoPlay: true
+    });
+});
